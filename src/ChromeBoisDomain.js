44a0604cdef305cff19235a0caf5e28b1d125ac1
Brinda Behal <brindaxo5@gmail.com>
2021-12-22 21:53:45 -0500

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -35,3 +35,34 @@ export default class ChromeBoisDomain extends Component {
     )
   }
 }
+import React, { Component } from 'react';
+import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js'
+
+
+export default class ChromeBoisDomain extends Component {
+  
+  handleMouseMove = (event) => {
+    drawChromeBoiAtCoords(event.clientX, event.clientY)
+  }
+  
+  handleKeyDown = (event) => {
+    if (event.key === 'a') {
+      resize('+')
+    } else if (event.key === 's') {
+      resize('-')
+    }
+  }
+   
+  render() {
+    return (
+      <canvas 
+        onMouseMove={this.handleMouseMove}
+        onClick={() => {toggleCycling()}}
+        onKeyDown={this.handleKeyDown}
+        width='900'
+        height='600'
+        tabIndex="0">
+      </canvas>
+    )
+  }
+}
